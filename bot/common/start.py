36549b430803b1de8ecda846ed64ce282efaad57
optikrus <optikrus61@gmail.com>
2023-01-22 21:16:45 +0300

    edit start handler

--- bot/common/start.py
+++ bot/common/start.py
@@ -1,7 +1,8 @@
 from aiogram.types import Message
 
 from bot.keyboards import MAIN_MENU
+from ..chat.constants import MAIN_MENU_MESSAGE
 
 
 async def send_main_menu(message: Message):
-    await message.bot.send_message(chat_id=message.chat.id, text="Main menu:", reply_markup=MAIN_MENU)
+    await message.bot.send_message(chat_id=message.chat.id, text=MAIN_MENU_MESSAGE, reply_markup=MAIN_MENU)
