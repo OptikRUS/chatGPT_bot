134e16accb368efa42d5daf1f0e51027ceace85e
optikrus <optikrus61@gmail.com>
2023-01-22 21:06:34 +0300

    add use_cases

---
+++ bot/chat/use_cases.py
@@ -0,0 +1,42 @@
+from aiogram.types import Message, CallbackQuery
+
+from bot.keyboards import ChatButtons
+from .utils import image_generation, text_generation, code_generation
+from .constants import (
+    image_generation_message, code_generation_message, text_generation_message,
+    GENERATE_TEXT_BUTTON, GENERATE_CODE_BUTTON, GENERATE_IMAGE_BUTTON,
+    WAIT_CODE, WAIT_TEXT, WAIT_IMAGE, WRONG_INPUT
+)
+
+
+async def process_output(inline_button: str, message: Message):
+    prompt: str = message.text
+    if not prompt:
+        await message.bot.send_message(chat_id=message.chat.id, text=WRONG_INPUT)
+    if inline_button == GENERATE_IMAGE_BUTTON:
+        await message.bot.send_message(text=WAIT_IMAGE, chat_id=message.chat.id)
+        image_url = await image_generation(prompt)
+        await message.bot.send_photo(chat_id=message.chat.id, photo=image_url)
+    elif inline_button == GENERATE_TEXT_BUTTON:
+        await message.bot.send_message(text=WAIT_TEXT, chat_id=message.chat.id)
+        answer = await text_generation(prompt)
+        await message.bot.send_message(chat_id=message.chat.id, text=answer)
+    elif inline_button == GENERATE_CODE_BUTTON:
+        await message.bot.send_message(text=WAIT_CODE, chat_id=message.chat.id)
+        code = await code_generation(prompt)
+        await message.bot.send_message(chat_id=message.chat.id, text=code)
+
+
+async def button_selection(callback_query: CallbackQuery):
+    if callback_query.data == GENERATE_IMAGE_BUTTON:
+        await callback_query.bot.send_message(
+            chat_id=callback_query.message.chat.id, text=image_generation_message()
+        )
+    elif callback_query.data == GENERATE_TEXT_BUTTON:
+        await callback_query.bot.send_message(
+            chat_id=callback_query.message.chat.id, text=text_generation_message()
+        )
+    elif callback_query.data == GENERATE_CODE_BUTTON:
+        await callback_query.bot.send_message(
+            chat_id=callback_query.message.chat.id, text=code_generation_message()
+        )
