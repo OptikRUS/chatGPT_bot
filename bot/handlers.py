2b31f3b16f4a4433085cff76d5f41e29bb2837f6
optikrus <optikrus61@gmail.com>
2023-01-22 21:08:11 +0300

    add chat handlers

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,10 +1,12 @@
 
 def get_handlers() -> list:
     from .common import init_common_handlers
+    from .chat import init_chat_handlers
 
     all_handlers: list = list()
 
     # регистрация хэндлеров
     all_handlers.append(init_common_handlers)
+    all_handlers.append(init_chat_handlers)
 
     return all_handlers
