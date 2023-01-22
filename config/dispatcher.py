4b697d3ef5bc452235401c9d2c3606b3ae475d4c
optikrus <optikrus61@gmail.com>
2023-01-22 20:26:15 +0300

    edit dispatcher

--- config/dispatcher.py
+++ config/dispatcher.py
@@ -1,4 +1,5 @@
 from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 
 def init_dispatcher() -> Dispatcher:
@@ -6,7 +7,7 @@ def init_dispatcher() -> Dispatcher:
     from bot.handlers import get_handlers
 
     bot: Bot = Bot(**bot_config)
-    dispatcher: Dispatcher = Dispatcher(bot)
+    dispatcher: Dispatcher = Dispatcher(bot, storage=MemoryStorage())
 
     for register_handler in get_handlers():
         register_handler(dispatcher)
