dd6fe2f4b9792f57cd6b12acae675d96f4b340b0
optikrus <optikrus61@gmail.com>
2023-01-29 01:11:17 +0300

    edit wrappers

--- bot/chat/wrappers.py
+++ bot/chat/wrappers.py
@@ -1,14 +1,16 @@
+from typing import Callable
+
 from aiogram.types import Message
 
 from .constants import API_ERROR
 
 
-def api_exceptions(func):
+def api_exceptions(api_case: Callable):
     async def wrapper(*args, **kwargs):
         try:
-            await func(*args, **kwargs)
+            await api_case(*args, **kwargs)
         except Exception as e:
-            # параметр e для будущих логов
+            # параметр "e" для будущих логов
             for arg in args:
                 if arg.__class__ == Message:
                     await arg.reply(text=API_ERROR + str(e))
