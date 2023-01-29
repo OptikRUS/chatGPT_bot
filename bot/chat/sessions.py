2172ab5aba104f0b126d700f60a82f50e0618ccd
optikrus <optikrus61@gmail.com>
2023-01-29 19:19:08 +0300

    edit create_session

--- bot/chat/sessions.py
+++ bot/chat/sessions.py
@@ -1,5 +1,6 @@
 import aiohttp
-from aiogram.types import Message
+from aiogram.types import Message, ParseMode
+from aiohttp import ClientResponseError
 
 from .exceptions import create_log
 
@@ -7,10 +8,14 @@ from .exceptions import create_log
 async def create_session(request_data: dict, message: Message) -> dict:
     async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False)) as request:
         async with request.post(**request_data) as response:
-            result = await response.json()
             if response.status == 400:
+                result = await response.json()
                 error_message = result.get("error").get("message")
                 create_log(message=message, error=result, answer=error_message)
                 await message.reply(text=error_message)
+            elif response.status == 502:
+                result = await response.text()
+                await message.reply(text=result, parse_mode=ParseMode.HTML)
+                create_log(message=message, error=ClientResponseError, answer=result)
             else:
-                return result
+                return await response.json()
