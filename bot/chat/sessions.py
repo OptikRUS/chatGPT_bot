1bbe82d2315b9322d0f87ed9e946131748a6ddd5
optikrus <optikrus61@gmail.com>
2023-01-29 23:06:08 +0300

    moved create_log

--- bot/chat/sessions.py
+++ bot/chat/sessions.py
@@ -11,8 +11,8 @@ async def create_session(request_data: dict, message: Message) -> dict:
             if response.status == 400:
                 result = await response.json()
                 error_message = result.get("error").get("message")
-                await create_log(message=message, error=result, answer=error_message)
                 await message.reply(text=error_message)
+                await create_log(message=message, error=result, answer=error_message)
             elif response.status == 502:
                 result = await response.text()
                 print("===502===")
