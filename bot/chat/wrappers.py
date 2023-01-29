ee710a1c0c900fd52bc7d522894ddc1a886139f8
optikrus <optikrus61@gmail.com>
2023-01-29 05:29:33 +0300

    add logs

--- bot/chat/wrappers.py
+++ bot/chat/wrappers.py
@@ -2,16 +2,21 @@ from typing import Callable
 
 from aiogram.types import Message
 
-from .constants import API_ERROR
+from .exceptions import parse_error, parse_log
 
 
 def api_exceptions(api_case: Callable) -> Callable:
     async def wrapper(*args, **kwargs) -> None:
         try:
             await api_case(*args, **kwargs)
-        except Exception as e:
-            # параметр "e" для будущих логов
-            for arg in args:
-                if arg.__class__ == Message:
-                    await arg.reply(text=API_ERROR + str(e))
+        except Exception as error:
+            error_message: str = parse_error(error=error)
+            if kwargs:
+                parse_log(message=kwargs)
+                await kwargs.get('message').reply(text=error_message)
+            elif args:
+                for arg in args:
+                    if arg.__class__ == Message:
+                        parse_log(message=arg)
+                        await arg.reply(text=error_message)
     return wrapper
