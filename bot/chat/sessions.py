72eb1ca61e3fc0e0e1983e3619aa6fabbc1feeac
optikrus <optikrus61@gmail.com>
2023-01-29 14:19:43 +0300

    add log to create_session

--- bot/chat/sessions.py
+++ bot/chat/sessions.py
@@ -1,6 +1,8 @@
 import aiohttp
 from aiogram.types import Message
 
+from .exceptions import create_log
+
 
 async def create_session(request_data: dict, message: Message) -> dict:
     async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False)) as request:
@@ -8,6 +10,7 @@ async def create_session(request_data: dict, message: Message) -> dict:
             result = await response.json()
             if response.status == 400:
                 error_message = result.get("error").get("message")
+                create_log(message=message, error=result, answer=error_message)
                 await message.reply(text=error_message)
             else:
                 return result
