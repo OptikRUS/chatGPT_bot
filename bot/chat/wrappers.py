16dfcffd50d565e1e95c88925c4f624d4edbea74
optikrus <optikrus61@gmail.com>
2023-01-29 23:04:34 +0300

    ass api_case name to message

--- bot/chat/wrappers.py
+++ bot/chat/wrappers.py
@@ -12,6 +12,7 @@ def api_exceptions(api_case: Callable) -> Callable:
         except Exception as error:
             error_message_for_user: str = parse_error(error=error)
             message = kwargs.get("message") or next((arg for arg in args if isinstance(arg, Message)), None)
-            await create_log(message=message, error=error, answer=error_message_for_user)
             await message.reply(text=error_message_for_user)
+            message.values["api_case"] = api_case.__name__
+            await create_log(message=message, error=error, answer=error_message_for_user)
     return wrapper
