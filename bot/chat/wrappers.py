368ab8941b79fee197039d43f5dd8c7ed033177a
optikrus <optikrus61@gmail.com>
2023-01-29 14:20:00 +0300

    improve api_exceptions

--- bot/chat/wrappers.py
+++ bot/chat/wrappers.py
@@ -2,7 +2,7 @@ from typing import Callable
 
 from aiogram.types import Message
 
-from .exceptions import parse_error, parse_log
+from .exceptions import parse_error, create_log
 
 
 def api_exceptions(api_case: Callable) -> Callable:
@@ -11,12 +11,7 @@ def api_exceptions(api_case: Callable) -> Callable:
             await api_case(*args, **kwargs)
         except Exception as error:
             error_message_for_user: str = parse_error(error=error)
-            if kwargs:
-                parse_log(message=kwargs, error=error, answer=error_message_for_user)
-                await kwargs.get('message').reply(text=error_message_for_user)
-            elif args:
-                for arg in args:
-                    if arg.__class__ == Message:
-                        parse_log(message=arg, error=error, answer=error_message_for_user)
-                        await arg.reply(text=error_message_for_user)
+            message = kwargs.get("message") or next((arg for arg in args if isinstance(arg, Message)), None)
+            create_log(message=message, error=error, answer=error_message_for_user)
+            await message.reply(text=error_message_for_user)
     return wrapper
