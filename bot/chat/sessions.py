f94d2873cd01a923475874ac96cd414ba8767748
optikrus <optikrus61@gmail.com>
2023-01-30 20:56:06 +0300

    add raises

--- bot/chat/sessions.py
+++ bot/chat/sessions.py
@@ -1,6 +1,6 @@
 import aiohttp
 from aiogram.types import Message, ParseMode
-from aiohttp import ClientResponseError
+from aiohttp.web import HTTPBadRequest, HTTPTooManyRequests, HTTPBadGateway
 
 from .exceptions import create_log
 
@@ -9,21 +9,24 @@ async def create_session(request_data: dict, message: Message) -> dict:
     async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False)) as request:
         async with request.post(**request_data) as response:
             if response.status == 400:
-                result = await response.json()
-                error_message = result.get("error").get("message")
+                result: dict = await response.json()
+                error_message: str = result.get("error").get("message")
                 await message.reply(text=error_message)
                 await create_log(message=message, error=result, answer=error_message)
+                raise HTTPBadRequest
             elif response.status == 502:
-                result = await response.text()
+                result: str = await response.text()
                 print("===502===")
                 print(result)
                 await message.reply(text=result, parse_mode=ParseMode.HTML)
-                await create_log(message=message, error=ClientResponseError, answer=result)
+                await create_log(message=message, error=result, answer=result)
+                raise HTTPBadGateway
             if response.status == 429:
-                result = await response.json()
-                error_message = result.get("error").get("message")
+                result: dict = await response.json()
+                error_message: str = result.get("error").get("message")
                 await message.reply(text=error_message)
                 await create_log(message=message, error=result, answer=error_message)
+                raise HTTPTooManyRequests
 
             else:
                 return await response.json()
