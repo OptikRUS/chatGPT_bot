576d852f54c62bb480e53d77dd4c97e8ed1c6b87
optikrus <optikrus61@gmail.com>
2023-01-31 00:19:16 +0300

    edit logging

---
+++ bot/chat/logger/logs.py
@@ -0,0 +1,28 @@
+import logging
+from logging import StreamHandler, FileHandler
+
+from aiogram.types import Message
+
+from config import chat_config
+
+
+console_handler: StreamHandler = logging.StreamHandler()
+file_handler: FileHandler = logging.FileHandler("logs/exceptions.log")
+
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    level=logging.ERROR,
+    handlers=[console_handler, file_handler]
+)
+
+
+async def create_log(message: Message, error: Exception | dict, answer: str) -> None:
+    msg: dict = dict(
+        error=error,
+        answer_to_user=answer,
+        message=message
+    )
+    logging.error(msg=msg)
+    await message.bot.send_message(chat_id=chat_config.get("log_chat_id"), text=msg)
