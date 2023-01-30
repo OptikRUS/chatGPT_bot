7bd7dc7af61ff13f5ea058694e6aa997db7e0ed2
optikrus <optikrus61@gmail.com>
2023-01-30 20:05:27 +0300

    add catch 429 status

--- bot/chat/sessions.py
+++ bot/chat/sessions.py
@@ -19,5 +19,11 @@ async def create_session(request_data: dict, message: Message) -> dict:
                 print(result)
                 await message.reply(text=result, parse_mode=ParseMode.HTML)
                 await create_log(message=message, error=ClientResponseError, answer=result)
+            if response.status == 429:
+                result = await response.json()
+                error_message = result.get("error").get("message")
+                await message.reply(text=error_message)
+                await create_log(message=message, error=result, answer=error_message)
+
             else:
                 return await response.json()
